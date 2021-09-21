c3ec8ad34f4d5e3d7ab3570d723b77e18aaf0644
Yash <yashshah2015@gmail.com>
2021-09-21 16:50:02 +0100

    Initial Commit

---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Yash Shah on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+			RPCView()
+        }
+    }
+}
